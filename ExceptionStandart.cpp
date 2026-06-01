a314fc4e6af0751f99c219d344329f1c28f99202
“WapaTzy” <wafarozanu22@gmail.com>
2026-06-01 18:42:51 +0700

    membuat penanda 1 (awal program)

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
 
+int main()
 {
-    cout << "Awal program" << endl; //penanda 1
-    try
+   
     {
         //deklarasi array data dengan panjang index 3
         array <int, 3> data = { 1, 2, 3 };
